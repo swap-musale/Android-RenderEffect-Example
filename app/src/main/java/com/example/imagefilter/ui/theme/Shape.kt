90a13db738b7d1002f9188dc3edd149ac4e5078c
Swapnil Musale <swapnil.musale@mutualmobile.com>
2023-03-15 22:39:02 +0530

    Initial Project SetUp

---
+++ app/src/main/java/com/example/imagefilter/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.imagefilter.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(size = 4.dp),
+    medium = RoundedCornerShape(size = 4.dp),
+    large = RoundedCornerShape(size = 0.dp),
+)
---
+++ app/src/main/java/com/example/imagefilter/ui/theme/Theme.kt
@@ -0,0 +1,38 @@
+package com.example.imagefilter.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+)
+
+@Composable
+fun ImageFilterTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content,
+        )
+    }
+}
